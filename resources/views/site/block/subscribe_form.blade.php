f4755627a7ca1ab4fa0f44f67f5b627f93e27a44
venodon <venodon@gmail.com>
2020-05-18 11:23:11 +0300

    assets

--- resources/views/site/block/subscribe_form.blade.php
+++ resources/views/site/block/subscribe_form.blade.php
@@ -1,15 +1,15 @@
 <div class="flex-group">
     <div class="input-holder first_name-field" style="margin-right: 1px;">
-        <input type="text" name="first_name" placeholder="Name"/>
+        <input type="text" name="first_name" placeholder="@lang('main.your_name')"/>
     </div>
     <div class="input-holder last_name-field" style="margin-right: 1px;">
-        <input type="text" name="last_name" placeholder="Last name"/>
+        <input type="text" name="last_name" placeholder="@lang('main.your_surname')"/>
     </div>
     <div class="input-holder phone-field" style="margin-right: 1px;">
-        <input type="text" name="phone" placeholder="Phone"/>
+        <input type="text" name="phone" placeholder="@lang('main.phone')"/>
     </div>
     <div class="input-holder mail-field" style="margin-right: 1px;">
-        <input type="email" name="email" placeholder="Email"/>
+        <input type="email" name="email" placeholder="@lang('main.email')"/>
     </div>
 
     <input type="hidden" name="current_url" value="{{ \URL::full() }}"/>
